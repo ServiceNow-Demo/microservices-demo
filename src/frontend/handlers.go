2159f645622c45ecdc225be0a043413c728939ae
Danny Smith <daniel.smith@servicenow.com>
2021-03-02 05:20:23 +0000

    Updated handlers.go

--- src/frontend/handlers.go
+++ src/frontend/handlers.go
@@ -112,7 +112,7 @@ func (plat *platformDetails) setPlatformDetails(env string) {
 		plat.provider = "Azure"
 		plat.css = "azure-platform"
 	} else {
-		plat.provider = "Pepsi"
+		plat.provider = "Google Cloud"
 		plat.css = "gcp-platform"
 	}
 }
